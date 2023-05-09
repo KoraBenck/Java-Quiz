b191becde2c0733331569363325da7690749b513
KoraBenck <123706880+KoraBenck@users.noreply.github.com>
2023-05-09 12:48:30 -0600

    added timer

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function countdown() {
+    var timeLeft = 5;
+  
+    var timeInterval = setInterval(function () {
+      if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+        displayMessage();
+      }
+    }, 1000);
+  }
+
 var questions = [
     {
         question: "What is Han Solo's ship called?",
