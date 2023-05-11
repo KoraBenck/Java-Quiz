ce69a1f7d1716e980e3b2be0fad3aec463d39163
KoraBenck <123706880+KoraBenck@users.noreply.github.com>
2023-05-11 12:39:25 -0600

    Redid code entirely. Added CSS

---
+++ assets/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+  numb: 1,
+  question: "What is the name of Han Solo's ship?",
+  answer: "The Millennium Falcon",
+  options: [
+    "Eagle 5",
+    "The Millennium Falcon",
+    "The Eon Vulture",
+    "The Decade Hawk"
+  ]
+},
+  {
+  numb: 2,
+  question: "Who is Luke's father revealed to be?",
+  answer: "Darth Vader",
+  options: [
+    "Han Solo",
+    "Ben Kenobi",
+    "Chewbacca",
+    "Lando Calrissian"
+  ]
+},
+  {
+  numb: 3,
+  question: "What color lightsaber to Obi-wan wield?",
+  answer: "Blue",
+  options: [
+    "Purple",
+    "Red",
+    "Yellow",
+    "blue"
+  ]
+},
+  {
+  numb: 4,
+  question: "Where did Lando Calrissian betray Han Solo?",
+  answer: "Cloud City",
+  options: [
+    "Cloud City",
+    "Mos Espa",
+    "Genosis",
+    "Hoth"
+  ]
+},
+  {
+  numb: 5,
+  question: "What does Leia say to Luke when she meets him?",
+  answer: "Aren't you too short for a Stormtrooper?",
+  options: [
+    "You're my only hope",
+    "I have a bad feeling about this",
+    "Aren't you too short for a Stormtrooper?",
+    "Fear is a path to the dark side."
+  ]
+},
+];
